a0836c4fe2ad77cd8183025c53d57ae09698e212
LeGend <98607285+LeGend077@users.noreply.github.com>
2023-06-27 11:03:58 +0530

    Add files via upload

---
+++ Chest UI - Server Form/BP/scripts/extensions/forms.d.ts
@@ -0,0 +1,34 @@
+import { Player } from "@minecraft/server";
+import { ActionFormResponse } from "@minecraft/server-ui";
+
+declare class ChestFormData {
+	/**
+	 * @param size The size of the chest. Can be 'small' or 'large'.
+	 */
+	constructor(size?: string);
+	/**
+	 * @remarks This builder method sets the title for the chest ui.
+	 * @param text The title text for the chest ui.
+	 */
+	title(text: string): ChestFormData;
+	/**
+	 * @remarks Adds a button to this chest ui with an icon from a resource pack.
+	 * @param itemName The name of the item to display.
+	 * @param itemDesc The item's lore to display.
+	 * @param iconPath The icon for the item.
+	 */
+	button(itemName: string, itemDesc?: string[], iconPath?: string): ChestFormData;
+	/**
+	  * @remarks
+	  * Creates and shows this modal popup form. Returns
+	  * asynchronously when the player confirms or cancels the
+	  * dialog.
+	  *
+	  * This function can't be called in read-only mode.
+	  *
+	  * @param player
+	  * Player to show this dialog to.
+	 */
+	show(player: Player): Promise<ActionFormResponse>;
+}
+export { ChestFormData };
--- Chest UI - Server Form/BP/scripts/index.js
+++ Chest UI - Server Form/BP/scripts/index.js
@@ -1,15 +1,15 @@
-import { system, world, Player } from '@minecraft/server'
-import { ChestFormData } from "forms.js"
+import { world } from '@minecraft/server'
+import { ChestFormData } from './extensions/forms.js';
 
 world.afterEvents.itemUse.subscribe(event => {
-     if (event.itemStack.typeId == "minecraft:compass" && event.itemStack.nameTag == "Teleporter") {
-
-          let form = new ChestFormData()
-          .title("LeGend Form")
-          .button("Test Item", ["test lore", "test lore 1"], "textures/items/apple", 4)
-          .show(event.source).then(response => {
-               console.warn(response.selection)
-          })
-          
-     } else { return; }
-})
+	const player = event.source;
+	if (event.itemStack.typeId == "minecraft:compass") {
+		const form = new ChestFormData('large')
+			.title('Testing')
+			.button('Test Item', ['Test lore', 'Test lore 2'], 'textures/items/stick', 4)
+			.show(player).then(response => {
+				if (response.canceled) return;
+				console.warn(response.selection)
+			})
+	}
+});
